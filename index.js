6ca68c803770149de0d92265ac11d8060de32285
Iasmim Rodrigues <iasmimrodrigues1301@gmail.com>
2023-06-07 19:25:51 -0300

    Fórmula intervalo de confiança - média corrigido

--- index.js
+++ index.js
@@ -1,33 +1,41 @@
-function intConfMed() {
+document.getElementById("btnSubmit").onclick = function () {
+  let radios = document.getElementsByName("opcoes");
+  let opcaoRadio;
+  for (let i = 0; i < radios.length; i++) {
+    if (radios[i].checked) {
+      opcaoRadio = radios[i].value;
+      calcular(opcaoRadio);
+    }
+  }
+};
+
+function calcular(opcaoRadio) {
   let media = Number(document.getElementById("media").value);
   // let grauConf = Number(document.getElementById('grauConf').value);
   let desvPad = Number(document.getElementById("desvPad").value);
   let tamAmostra = Number(document.getElementById("tamAmostra").value);
+  let grauConf;
 
-  document.getElementById("btnSubmit").onclick = function () {
-    let radios = document.getElementsByName("opcoes");
-    for (let i = 0; i < radios.length; i++) {
-      if (radios[i].checked) {
-        let grauConf = radios[i].value;
-      }
-    }
-  };
+  if (opcaoRadio == 90) {
+    grauConf = 1.645;
+  } else if (opcaoRadio == 95) {
+    grauConf = 1.96;
+  } else {
+    grauConf = 2.575;
+  }
 
   let raiz = Math.sqrt(tamAmostra);
   let erro = (grauConf * desvPad) / raiz;
 
-  let arredondar = parseFloat(erro.toFixed(2)); // para aproximar o num
+  let arredondar = parseFloat(erro.toFixed(2)); // para aproximar o número
 
   let intervaloMenos = media - arredondar;
   let intervaloMais = media + arredondar;
 
-
   document.getElementById("resultado").innerHTML =
-    "<h2> Intervalo de confiança: " +
+    "<h2>Intervalo de confiança: " +
     intervaloMenos +
     " < μ < " +
     intervaloMais +
-    " </h2>";
-
-  // console.log(media, grauConf, desvPad, tamAmostra)
+    "</h2>";
 }
